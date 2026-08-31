c98a7a770d19af79c595b2b9500f5ee6152db420
ali89akbar <alia42268@gmail.com>
2026-09-01 00:25:06 +0500

    heatmap functionality implemented

--- PlantWise-Project/loginPage/Plant-Wise-Plant-Disease-Detection-/client/src/App.js
+++ PlantWise-Project/loginPage/Plant-Wise-Plant-Disease-Detection-/client/src/App.js
@@ -3,14 +3,14 @@ import Home from './Components/Home';
 import Headers from './Components/Headers';
 import Login from './Components/Login';
 import Dashboard from './Components/Dashboard';
-import SavedPlants from './Components/SavedPlants'; // Import the new component
+import SavedPlants from './Components/SavedPlants';
+import OutbreakRadar from './Components/OutbreakRadar';
 import Error from './Components/Error';
 import { Routes, Route } from 'react-router-dom';
 import SocialMedia from './Components/SocialMedia';
 import BadgeProgressPage from './Components/BadgeProgressPage';
 import ARViewer from './Components/ARViewer';
 import PlantGallery from './Components/PlantGallery';
-
 
 function App() {
     return (
@@ -20,8 +20,9 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/outbreak-radar" element={<OutbreakRadar />} />
                 <Route path="/social-media" element={<SocialMedia />} /> 
-                <Route path="/saved-plants" element={<SavedPlants />} /> {/* Add this route */}
+                <Route path="/saved-plants" element={<SavedPlants />} />
                 <Route path="/badge-progress" element={<BadgeProgressPage />} />
                 <Route path='/ar' element={<PlantGallery />}/>
                 <Route path='/ar-viewer' element={<ARViewer />}/>
